60079443902a9204c10d5000d78b57a5e88d7d77
leo <leo@leodeMacBook-Pro.local>
2021-08-03 17:31:50 +0800

    update

--- assets/koa_static/chat.js
+++ assets/koa_static/chat.js
@@ -5,6 +5,18 @@ window.mobileCheck = function() {
     return check;
   };
 
+
+function inputcheck(element)
+{
+    var max_chars = 100;
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
+    }
+    if(event.keyCode == 13) {
+        $('#chatsend').click();
+    }
+}
+
 $(document).ready(function(){
 
     var div=$("#chatwindow");
@@ -14,11 +26,11 @@ $(document).ready(function(){
 
         var bottombox=$("<div>",{"style":"height:100px;width:100%;background-color:#101012;position:absolute;top:0px;box-shadow: 2px 0px 0px 2px #303033;"});
         
-        var inputbox=$("<input>",{"id":"chatinput","style":"border-radius: 10px; padding: 5px 20px; height: 40px; background-color: #1b1b1d; border: 0px; margin: 18px; font-size: 28px;  width: 60%; color: white; display: inline-block; float: right; font-size: 23px;"});
+        var inputbox=$("<input>",{"id":"chatinput","onkeydown":"inputcheck(this)", "style":"border-radius: 10px; padding: 5px 20px; height: 40px; background-color: #1b1b1d; border: 0px; margin: 18px; font-size: 28px;  width: 60%; color: white; display: inline-block; float: right; font-size: 23px;"});
         
         var btn=$('<div>',{"id":"chatsend","style":"font-size: 35px;margin: 12px 20px 0px 3px;padding: 5px 15px;background-color: #1b3896;border-radius:5px;color:#fdfdfd;cursor:pointer;text-align:center;float:right;width:100px"});
          
-        
+ 
         btn.html("send");
 
         bottombox.append(btn);
@@ -37,7 +49,7 @@ $(document).ready(function(){
 
         var bottombox=$("<div>",{"style":"height:54px;width:100%;background-color:#101012;position:absolute;bottom:0px;box-shadow: 2px 0px 0px 2px #303033;"});
         
-        var inputbox=$("<input>",{"id":"chatinput","style":"height: 30px;background-color: #2f2f2f;border:0px;margin:10px;font-size: 14px;width: 260px;color:white;display: inline-block;"});
+        var inputbox=$("<input>",{"id":"chatinput","onkeydown":"inputcheck(this)", "style":"height: 30px;background-color: #2f2f2f;border:0px;margin:10px;font-size: 14px;width: 260px;color:white;display: inline-block;"});
         
         var btn=$('<div>',{"id":"chatsend","style":"font-size: 16px;margin: 5px 0px 0px 3px;padding: 5px 15px;background-color: #1b3896;border-radius:5px;color:#fdfdfd;display:inline-block;cursor:pointer;text-align:center"});
         btn.html("send");
